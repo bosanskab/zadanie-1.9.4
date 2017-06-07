d3250a2db988b883106ce6d44e39923722bf19af
bosanskab <bosanska.bar@gmail.com>
2017-06-07 14:18:08 +0200

    Add files via upload

--- zadanie 2- 1.9.4..c
+++ zadanie 2- 1.9.4..c
@@ -13,29 +13,30 @@ typedef struct{
 	float y;
 }VECTOR;
 
+
 typedef struct{
-	float x;
-	float y;
-	float vseob;
-	float smernica;
+	float a;
+	float b;
+	float c;
 }LINE;
 
 void randomAB(POINT *A, POINT *B)
 {
+	
+	srand(time(NULL));
+	
 	A->x=-5+rand()%10;
 	A->y=-5+rand()%10;
 	B->x=-5+rand()%10;
 	B->y=-5+rand()%10;	
 		
-	return 0;
 }
 
-void pointC (POINT *A, POINT *B,POINT *C)
+void pointC (POINT *A,POINT *B,POINT *C)
 {	
 	C->x=((A->x)+(B->x))/2;
 	C->y=((A->y)+(B->y))/2;
 	
-	return 0;
 }									
 
 char vectorAB (POINT *A, POINT *B, VECTOR *v,VECTOR *n)
@@ -48,18 +49,25 @@ char vectorAB (POINT *A, POINT *B, VECTOR *v,VECTOR *n)
 	return 0;
 }
 
-char point_point_axis(POINT *A, POINT *B,POINT *C, LINE *a,VECTOR *v,VECTOR *n)
+char point_point_axis(POINT *A, POINT *B, LINE *p)
 {
-	float c;
+	float smernica,vseobecna,paramx,paramy;
 	int vysledok;
 	char t,x,y;
-
-	c=-((C->x)*(v->x)+(C->y)*(v->y));
+	POINT C;
+	VECTOR v,n;
+	
+	pointC (&A,&B,&C);
+	vectorAB (&A,&B,&v,&n);	
+	
+	p->a=(v.x);
+	p->b=(v.y);
+	p->c=-((C.x)*(p->a)+(C.y)*(p->b));
 		
-	a->x=(C->x)+(n->x)*t;
-	a->y=(C->y)+(n->y)*t;
-	a->vseob=(v->x)*x+(v->y)*y+c;
-	a->smernica=((v->x)*x+c)/-(v->y);
+	paramx=(C.x)+(n.x)*t;
+	paramy=(C.y)+(n.y)*t;
+	vseobecna=(p->a)*x+(p->b)*y+(p->c);
+	smernica=((p->a)*x+(p->c))/-(p->b);
 
     if ((A->x)==(B->x)&&(A->y)==(B->y))
         vysledok=0;
@@ -70,16 +78,12 @@ char point_point_axis(POINT *A, POINT *B,POINT *C, LINE *a,VECTOR *v,VECTOR *n)
 
 
 main(){
-	srand(time(NULL));
-	POINT A,B,C;
-	VECTOR v,n;
-	LINE a;
-
+	POINT A,B;
+	LINE p;
+	
 	randomAB(&A,&B);
-	pointC (&A,&B,&C);
-	vectorAB (&A,&B,&v,&n);	
 
-	if (point_point_axis(&A,&B,&C,&a,&v,&n)==1)
+	if (point_point_axis(&A,&B,&p)==1)
        	printf("vypocet prebehol uspesne\n");
    		else printf(" vypocet zlyhal\n");
    
